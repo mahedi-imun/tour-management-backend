242db353a5cd3bbc3282e5ff49554eb75ea37abc
mahedi <mahedianik4@gmail.com>
2022-09-20 02:32:20 +0600

    solve deply error

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 function dbConnect(){
-  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASSWORD}@cluster0.fcuh9cy.mongodb.net/?retryWrites=true&w=majority`)
+  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fcuh9cy.mongodb.net/?retryWrites=true&w=majority`)
 }
 
 module.exports = dbConnect;
